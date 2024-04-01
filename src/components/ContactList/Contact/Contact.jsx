16f31fed79ce0cccc0ccdf8b079e1f3516390349
Volodymyr <volodymyrlesiv@ykr.net>
2024-04-01 12:20:17 +0300

    done

---
+++ src/components/ContactList/Contact/Contact.jsx
@@ -0,0 +1,29 @@
+import css from "./Contact.module.css";
+import { FaUser } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../../redux/contactsOps";
+
+export default function Contact({ contact }) {
+  const dispatch = useDispatch();
+
+  const handlerDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+
+  return (
+    <div className={css.card}>
+      <div className={css.contactData}>
+        <p>
+          <FaUser /> {contact.name}
+        </p>
+        <p>
+          <FaPhone /> {contact.number}
+        </p>
+      </div>
+      <button onClick={handlerDelete} className={css.btn}>
+        Delete
+      </button>
+    </div>
+  );
+}
